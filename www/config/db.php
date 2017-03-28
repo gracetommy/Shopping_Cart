3b4afa312c65572be433b028c6cbaf0379a2cd59
Grace <gracetitilope05@gmail.com>
2017-03-28 14:03:27 +0100

    db congi

--- www/config/db.php
+++ www/config/db.php
@@ -1,13 +1,11 @@
 <?php
 #test.php sandbox
 
-define('DBNAME', 'testing');
+/*define('DBNAME', 'testing');
 define('DBUSER', 'root');
 define('DBPASS', 'tommy');
 
-try{
-
-	#prepare  pdo instance
+try{#prepare  pdo instance
 $conn = new PDO('mysql:host=localhost;dbname='.DBNAME, DBUSER, DBPASS);
 
 #set verbose error modes
@@ -15,8 +13,12 @@ $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_SILENT);
 
 }catch(PDOException $e){
 	echo $e->getMessage();
-}
+}*/
 
-
+<form id="register" method="POST" enctype="multipart/form-data">
+<p>Please Upload a file</p>
+<input type="file" name="pic">
+<input type="submit" name="save">
+</form>
 
 ?>
