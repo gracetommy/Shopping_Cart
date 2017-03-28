914a2eebf375e8b01d56e42b25470a9b8f3bf5a1
Grace <gracetitilope05@gmail.com>
2017-03-28 13:55:12 +0100

    db connection file

---
+++ www/config/db.php
@@ -0,0 +1,22 @@
+<?php
+#test.php sandbox
+
+define('DBNAME', 'testing');
+define('DBUSER', 'root');
+define('DBPASS', 'tommy');
+
+try{
+
+	#prepare  pdo instance
+$conn = new PDO('mysql:host=localhost;dbname='.DBNAME, DBUSER, DBPASS);
+
+#set verbose error modes
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_SILENT);
+
+}catch(PDOException $e){
+	echo $e->getMessage();
+}
+
+
+
+?>
