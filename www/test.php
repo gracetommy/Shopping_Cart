37c10d374126792138b1168c96555b5bf33edb1b
Grace <gracetitilope05@gmail.com>
2017-03-28 13:23:27 +0100

    creating pdo instance and setting verbose error mode

--- www/test.php
+++ www/test.php
@@ -5,9 +5,17 @@ define('DBNAME', 'bookstore');
 define('DBUSER', 'root');
 define('DBPASS', 'romantic');
 
+try{
 
+	#prepare  pdo instance
 $conn = new PDO('mysql:host=localhost;dbname='.DBNAME, DBUSER, DBPASS);
 
+#set verbose error modes
+$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_SILENT);
+
+}catch(PDOException $e){
+	echo $e->getMessage();
+}
 
 
 
