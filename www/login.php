40f3953c036fe90113cf0d85c3e79cf30ce59de9
Grace <gracetitilope05@gmail.com>
2017-03-30 12:44:13 +0100

    validating the form

--- www/login.php
+++ www/login.php
@@ -6,6 +6,27 @@
 
 	#include head.php
 include 'config/head.php';
+
+if (array_key_exists('register', $_POST)){
+
+	#cache errors
+	$errors = [];
+
+	#validate firstname
+	if(empty($_POST['fname'])){
+		$errors[] = "Please enter a first name";
+	}
+
+	if(empty($errors )){
+		//do db stuff
+	} else{
+		foreach ($errors as err){
+			echo $err;
+		}
+	}
+
+
+}
 ?>
 	
 	<div class="wrapper">
